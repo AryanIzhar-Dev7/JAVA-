2571bf3e0f860c6cbc5030b5ad05b7f023c7a9b5
Aryan Izhar <aryanizhar2002@gmail.com>
2023-03-07 17:50:37 +0500

    Vehicle
    In this Program i have demonstrated Default Constructor

---
+++ Classes and Constructor/Vechicle.java
@@ -0,0 +1,27 @@
+class Vehicle {
+	String type;
+	String name;
+	int capacity;
+
+	// Creating a default Constructor and it is automatically called when object of class is created;
+	Vehicle(){
+				};
+
+}
+
+clas Vehicle{
+
+	public static void main (Strin args[]){
+	Vehicle vehicle = new Vehicle ();
+	
+	vehicle.type="Car";
+	vehicle.name="Toyota";
+	vehicle.capacity=4;
+
+	System.out.println("Type : "+vehicle.type+ "	 Name : " +vehicle.name+"   	Capacity : "+vehicle.capacity);
+	
+
+
+		}
+
+	}
